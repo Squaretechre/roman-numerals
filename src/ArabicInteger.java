0a50ce46186fa4fc33dc5bb007e9931ea0eb1b76
Daniel Parkin <danielparkin1@gmail.com>
2018-10-03 22:49:19 +0100

    refactor: generalise with lookup

--- src/ArabicInteger.java
+++ src/ArabicInteger.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ArabicInteger {
     private static final String NumeralFor1 = "I";
     private static final String NumeralFor5 = "V";
@@ -5,9 +9,23 @@ public class ArabicInteger {
     private static final String NumeralFor50 = "L";
     private static final String NumeralFor100 = "C";
     private final int integer;
+    private final Map<Integer, String> numerals;
 
     public ArabicInteger(int integer) {
         this.integer = integer;
+        this.numerals = new LinkedHashMap<Integer, String>() {
+            {
+                put(100, "C");
+                put(90, "XC");
+                put(50, "L");
+                put(40, "XL");
+                put(10, "X");
+                put(9, "IX");
+                put(5, "V");
+                put(4, "IV");
+                put(1, "I");
+            }
+        };
     }
 
     public String toRomanNumeral() {
@@ -15,40 +33,12 @@ public class ArabicInteger {
         int decreasingNumber = this.integer;
 
         while (decreasingNumber > 0) {
-            int currentNumberLookingAt = decreasingNumber;
-            int someBoundary = 100;
-            if (currentNumberLookingAt >= someBoundary) {
-                String theNumeralForThatBoundary = NumeralFor100;
-                decreasingNumber -= someBoundary;
-                numeralString +=  theNumeralForThatBoundary;
-            }
-            if (decreasingNumber >= 90) {
-                decreasingNumber -= 90;
-                numeralString += NumeralFor10 + NumeralFor100;
-            }
-            if (decreasingNumber >= 50) {
-                decreasingNumber -= 50;
-                numeralString += NumeralFor50;
-            }
-            if (decreasingNumber >= 40) {
-                decreasingNumber -= 40;
-                numeralString += NumeralFor10 + NumeralFor50;
-            }
-            if (decreasingNumber >= 10) {
-                decreasingNumber -= 10;
-                numeralString += NumeralFor10;
-            } else if (decreasingNumber >= 9) {
-                decreasingNumber -= 9;
-                numeralString += (NumeralFor1 + NumeralFor10);
-            } else if (decreasingNumber >= 5) {
-                decreasingNumber -= 5;
-                numeralString += NumeralFor5;
-            } else if (decreasingNumber >= 4) {
-                decreasingNumber -= 4;
-                numeralString += NumeralFor1 + NumeralFor5;
-            } else if (decreasingNumber >= 1) {
-                decreasingNumber -= 1;
-                numeralString += NumeralFor1;
+            for (Map.Entry<Integer, String> numeral : numerals.entrySet()) {
+                if (decreasingNumber >= numeral.getKey()) {
+                    decreasingNumber -= numeral.getKey();
+                    numeralString += numeral.getValue();
+                    break;
+                }
             }
         }
 
